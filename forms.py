38c9fe8a8f0f7180f3768959e8f0e20710ba4467
Dolan <dylanmulcahy19@gmail.com>
2023-01-26 14:54:16 +0000

    Removed an unnecessary import from forms.py

--- forms.py
+++ forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm#
 from wtforms import StringField, PasswordField, SubmitField, SelectField
-from wtforms.validators import InputRequired, EqualTo, NumberRange, Length
+from wtforms.validators import InputRequired, EqualTo, Length
 
 class RegistrationForm(FlaskForm):
     user_id = StringField("User ID:",validators=[InputRequired(),Length(min=4,max=20)])
