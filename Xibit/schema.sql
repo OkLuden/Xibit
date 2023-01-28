1edc16692792e2837d7bf7fb771321dafcae3702
darragh <120389806@umail.ucc.ie>
2023-01-28 16:55:52 +0000

    moved files into a package

---
+++ Xibit/schema.sql
@@ -0,0 +1,8 @@
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users
+(
+    'user_id' VARCHAR(30) PRIMARY KEY,
+    'password' TEXT NOT NULL,
+    'total_likes' INT
+);
--- schema.sql
+++
@@ -1,8 +0,0 @@
-DROP TABLE IF EXISTS users;
-
-CREATE TABLE users
-(
-    'user_id' VARCHAR(30) PRIMARY KEY,
-    'password' TEXT NOT NULL,
-    'total_likes' INT
-);
