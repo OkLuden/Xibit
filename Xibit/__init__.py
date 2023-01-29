e23b070013e59d2bf440be28bbce2e0e016b55b8
Dolan <dylanmulcahy19@gmail.com>
2023-01-29 18:03:05 +0000

    Added imports to __init__.py and page to base.html

--- Xibit/__init__.py
+++ Xibit/__init__.py
@@ -1,8 +1,15 @@
 from flask import Flask, render_template, url_for, redirect
+from forms import RegistrationForm, LoginForm
+from db import get_db, close_db
+from flask_session import Session
 
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "this-is-my-secret-key"
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 
 @app.route("/")
 def test():
-    return "Hello, World!"
+    return render_template("index.html" , page = "Home")
