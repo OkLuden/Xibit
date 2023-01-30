a21426ee1bfee5fbc85802078a0ccd54fb62c211
Dolan <dylanmulcahy19@gmail.com>
2023-01-30 15:08:59 +0000

    Added email form input to registration in forms.py

--- Xibit/forms.py
+++ Xibit/forms.py
@@ -3,7 +3,8 @@ from wtforms import StringField, PasswordField, SubmitField, SelectField
 from wtforms.validators import InputRequired, EqualTo, Length
 
 class RegistrationForm(FlaskForm):
-    user_id = StringField("User ID:",validators=[InputRequired(),Length(min=4,max=20)])
+    user_id = StringField("User ID:",validators=[InputRequired(),Length(min=4,max=30)])
+    email = StringField("Email:",validators=[InputRequired(),Length(min=4,max=254)])
     password = PasswordField("Password:",validators=[InputRequired(), Length(min=4,max=20)])
     password2 = PasswordField("Confirm Password:",validators=[InputRequired(), EqualTo("password")])
     submit = SubmitField("Submit")
