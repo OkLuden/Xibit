31e2256788c66d335eb1613d6bda39156457af65
Dolan <dylanmulcahy19@gmail.com>
2023-01-30 13:35:12 +0000

    Added to load logged in user and close db at end

--- Xibit/app.py
+++ Xibit/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, redirect
+from flask import Flask, render_template, url_for, redirect, request, jsonify, g, session, make_response
 from forms import RegistrationForm, LoginForm
 from db import get_db, close_db
 from flask_session import Session
@@ -9,6 +9,14 @@ app.config["SECRET_KEY"] = "this-is-my-secret-key"
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
+
+@app.teardown_appcontext
+def close_db_at_end_of_requests(e=None):
+    close_db(e)
+
+@app.before_request
+def load_logged_in_user():
+    g.user = session.get("user_id", None)
 
 @app.errorhandler(404)
 def page_not_found(error):
