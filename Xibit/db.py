e039a3520400d73919f90e6d9503ddc1c94d6f46
darragh <120389806@umail.ucc.ie>
2023-02-10 12:19:30 +0000

    added cursorclass to db.py

--- Xibit/db.py
+++ Xibit/db.py
@@ -1,14 +1,14 @@
 from flask import g
 import pymysql.connections as dbConnection
 
-
 def get_db():
     if "db" not in g:
         db = dbConnection.Connection(host='127.0.0.1', 
         user='root', 
         password='H1@l//C$rT', 
         database='XibitDB', 
-        port=server.local_bind_port)
+        port=server.local_bind_port
+        cursorclass=pymsql.cursors.DictCursor)
     return g.db
 
 def close_db(e=None):
